f42ff2d7ba096ceeff19d5c9e42b670a830f65f7
Abdullah al mamun <abdullah.ssc13@gmail.com>
2020-10-26 22:46:13 +0600

    routes list modify

--- routes/web.php
+++ routes/web.php
@@ -3,56 +3,73 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+/**********************************
+ * start frontend routes
+ **********************************/
+
 Route::get('/', 'HomeController@index')->name('home');
 
 Auth::routes();
 
-//newsletter
+// newsletter
 Route::post('newsletter/store', 'NewsLetterController@store')->name('newsletter.store');
 
+/**********************************
+ * end frontend routes
+ **********************************/
 
-//admin authentication
+
+
+
+/**********************************
+ * start admin routes
+ **********************************/
+
+// authentication
+Route::get('/admin', function () {
+    return redirect()->route('admin.login');
+});
+
 Route::group(['as' => 'admin.', 'prefix' => 'admin', 'namespace' => 'Admin'], function () {
-
-    Route::get('/', function () {
-        return redirect()->route('admin.login');
-    });
-
     Auth::routes();
 });
 
-//admin
+// admin
 Route::group(['middleware' => ['auth:admin'], 'as' => 'admin.', 'prefix' => 'admin', 'namespace' => 'Admin'], function () {
 
-    //dashboard
+    // dashboard
     Route::get('dashboard', function () {
         return view('admin.dashboard');
     })->name('dashboard');
 
-    //category
+    // category
     Route::get('categories/change-status/{category}', 'CategoryController@changeStatus')
         ->name('categories.status.change');
     Route::resource('categories', 'CategoryController');
 
-    //brand
+    // brand
     Route::get('brands/change-status/{brand}', 'BrandController@changeStatus')
         ->name('brands.status.change');
     Route::resource('brands', 'BrandController');
 
-    //product
+    // product
     Route::get('products/change-status/{product}', 'ProductController@changeStatus')
         ->name('products.status.change');
     Route::resource('products', 'ProductController');
 
-    //coupon
+    // coupon
     Route::get('coupons/change-status/{coupon}', 'CouponController@changeStatus')
         ->name('coupons.status.change');
     Route::resource('coupons', 'CouponController');
 
-    //newsletter
+    // newsletter
     Route::resource('newsletters', 'NewsLetterController')->only(['index', 'destroy']);
 
-    //password
+    // password
     Route::get('password-change', 'AdminController@changePassword')->name('password.change');
     Route::post('password-update', 'AdminController@updatePassword')->name('password.update');
 });
+
+/**********************************
+ * end admin authentication
+ **********************************/
