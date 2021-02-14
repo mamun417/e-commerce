0f916301d4bfe90abb532404f96c825b6dccf8ae
mamun417 <git-002@devxhub.com>
2021-02-15 01:17:16 +0600

    mix use for frontend css file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,18 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="OneTech shop project">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="{{ asset('frontend/styles/bootstrap4/bootstrap.min.css') }}">
-    <link href="{{ asset('frontend/plugins/fontawesome-free-5.0.1/css/fontawesome-all.css') }}" rel="stylesheet"
-          type="text/css">
-    <link rel="stylesheet" type="text/css" href="{{ asset('frontend/plugins/OwlCarousel2-2.2.1/owl.carousel.css') }} ">
-    <link rel="stylesheet" type="text/css"
-          href="{{ asset('frontend/plugins/OwlCarousel2-2.2.1/owl.theme.default.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('frontend/plugins/OwlCarousel2-2.2.1/animate.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('frontend/plugins/slick-1.8.0/slick.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('frontend/styles/main_styles.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('frontend/styles/responsive.css') }}">
-    <link href="{{ asset('backend/css/plugins/toastr/toastr.min.css') }}" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="{{ asset('frontend/styles/cus_style.css') }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon.ico') }}">
 
     @stack('extra-links')
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,16 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+mix.styles([
+    'resources/frontend/styles/bootstrap4/bootstrap.min.css',
+    'resources/frontend/plugins/fontawesome-free-5.0.1/css/fontawesome-all.css',
+    'resources/frontend/plugins/OwlCarousel2-2.2.1/owl.carousel.css',
+    'resources/frontend/plugins/OwlCarousel2-2.2.1/owl.theme.default.css',
+    'resources/frontend/plugins/OwlCarousel2-2.2.1/animate.css',
+    'resources/frontend/plugins/slick-1.8.0/slick.css',
+    'resources/frontend/styles/main_styles.css',
+    'resources/frontend/styles/responsive.css',
+    'resources/backend/css/plugins/toastr/toastr.min.css',
+    'resources/frontend/styles/cus_style.css',
+], 'public/css/frontend/style.css');
